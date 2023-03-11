016c6f7d0929262ae7f21b93426c77f10226839d
1209090 <1209090@gmail.com>
2023-03-11 14:35:12 +0600

    429 крупа

--- wordle.py
+++ wordle.py
@@ -1,4 +1,14 @@
 import csv
+labels = ['L', 'T', 'F', 'S']
+
+def to_int(s):
+    return int(s) if s.isdigit() else None
 
 with open('wordle.csv') as csvfile:
     rows = list(csv.DictReader(csvfile))
+
+with open('elo.csv') as elofile:
+    elo = list(csv.DictReader(elofile))
+
+def values(row):
+    return [to_int(row[name]) for name in labels]
