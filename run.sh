03d942820bc9b1b24d5820e10c5912a8dff9c36c
1209090 <1209090@gmail.com>
2024-05-11 22:46:40 +0600

    856 виски

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 /usr/bin/env ruby wordle.rb
 python3 wordle.py
+/usr/bin/env ruby -rcsv \
+  -e "data = CSV.readlines 'week-champs.csv', headers: true" \
+  -e "players = data.headers[2..-1]" \
+  -e "res = data.reduce(Hash.new(0)) { |acc, row| players.each { |k| acc[k] += row[k].to_i }; acc }" \
+  -e "puts res.sort_by(&:last).reverse.map { _1.join(' ') }.join(', ')"
