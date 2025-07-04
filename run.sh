c2c74c02cec31af3dfb7dc026353e4df262b12b8
1209090 <1209090@gmail.com>
2025-07-04 21:46:15 +0600

    1275 точка

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 python3 wordle.py
 /usr/bin/env ruby -rcsv \
+  -e "def average(array) = array.sum.to_f / array.size" \
+  -e "def pretty_print(array) = array.sort_by(&:last).reverse.map { _1.join(' ') }.join(', ')" \
   -e "data = CSV.readlines 'stats.csv', headers: true" \
   -e "players = data.headers[2..-1]" \
   -e "res = data.reduce(Hash.new(0)) { |acc, row| players.each { |k| acc[k] += row[k].to_i }; acc }" \
-  -e "puts res.sort_by(&:last).reverse.map { _1.join(' ') }.join(', ')"
+  -e "puts pretty_print(res)" \
+  -e "data = CSV.read('wordle.csv', headers: true)" \
+  -e "res = data.headers[2..].map { |k| [k, average(data[k].compact.map(&:to_i)).round(2)] }" \
+  -e "puts pretty_print(res)"
